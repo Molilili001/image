a9e23e6f68983665a97669060a7d59fda7422bba
Molilili <lyx99633@gmail.com>
2025-08-15 19:58:55 +0800

    Add files via upload

---
+++ character_data.js
@@ -0,0 +1,22 @@
+// character_data.js
+
+// 将您的角色数据存储在一个全局变量中。
+// 这是一个以角色名为键的对象，方便直接查找。
+var myRemoteCharacterData = {
+  "洛千霜": {
+    // 您可以在这里为“洛千霜”添加图片或其他数据
+    // 例如: "sfw": { "默认": "url_here" }, "nsfw": {}
+  },
+  "林语惊": {
+    "nsfw": {
+      "精液": "https://gitgud.io/Molilili/images/-/raw/master/%E6%9E%97%E8%AF%AD%E6%83%8A/nsfw/1.png",
+      "踩头": "https://gitgud.io/Molilili/images/-/raw/master/%E6%9E%97%E8%AF%AD%E6%83%8A/nsfw/2.png"
+    },
+    "sfw": {
+      // 您可以在这里为“林语惊”添加SFW图片
+    }
+  },
+  "陈予欢": {
+    // 您可以在这里为“陈予欢”添加图片或其他数据
+  }
+};
