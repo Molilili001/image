57bd14802bd92ed39fd5f8bf8d79272ff0b6acac
Molilili <lyx99633@gmail.com>
2025-08-15 20:05:50 +0800

    Update character_data.js

--- character_data.js
+++ character_data.js
@@ -1,22 +1,30 @@
-// character_data.js
+// [DEBUG] character_data.js - v2
+console.log('[Remote Data Script] Starting execution of character_data.js...');
 
-// 将您的角色数据存储在一个全局变量中。
-// 这是一个以角色名为键的对象，方便直接查找。
-var myRemoteCharacterData = {
-  "洛千霜": {
-    // 您可以在这里为“洛千霜”添加图片或其他数据
-    // 例如: "sfw": { "默认": "url_here" }, "nsfw": {}
-  },
-  "林语惊": {
-    "nsfw": {
-      "精液": "https://gitgud.io/Molilili/images/-/raw/master/%E6%9E%97%E8%AF%AD%E6%83%8A/nsfw/1.png",
-      "踩头": "https://gitgud.io/Molilili/images/-/raw/master/%E6%9E%97%E8%AF%AD%E6%83%8A/nsfw/2.png"
+try {
+  // 使用 'var' 在顶层作用域声明，以确保它成为全局变量
+  var myRemoteCharacterData = {
+    "洛千霜": {
+      // 您可以在这里为“洛千霜”添加图片或其他数据
     },
-    "sfw": {
-      // 您可以在这里为“林语惊”添加SFW图片
+    "林语惊": {
+      "nsfw": {
+        "精液": "https://gitgud.io/Molilili/images/-/raw/master/%E6%9E%97%E8%AF%AD%E6%83%8A/nsfw/1.png",
+        "踩头": "https://gitgud.io/Molilili/images/-/raw/master/%E6%9E%97%E8%AF%AD%E6%83%8A/nsfw/2.png"
+      },
+      "sfw": {
+        // 您可以在这里为“林语惊”添加SFW图片
+      }
+    },
+    "陈予欢": {
+      // 您可以在这里为“陈予欢”添加图片或其他数据
     }
-  },
-  "陈予欢": {
-    // 您可以在这里为“陈予欢”添加图片或其他数据
-  }
-};
+  };
+
+  console.log('[Remote Data Script] SUCCESS: myRemoteCharacterData has been created.');
+  // 打印变量内容以供检查
+  console.log(myRemoteCharacterData);
+
+} catch (error) {
+  console.error('[Remote Data Script] ERROR: An error occurred during script execution.', error);
+}
